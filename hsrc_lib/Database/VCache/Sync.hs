f5609b0ba7858021a24788560428d9d2b8e3f21b
David Barbour <dmbarbour@gmail.com>
2015-01-28 11:06:37 -0600

    simplified implementation

--- hsrc_lib/Database/VCache/Sync.hs
+++ hsrc_lib/Database/VCache/Sync.hs
@@ -4,27 +4,16 @@ module Database.VCache.Sync
     ( vcacheSync
     ) where
 
-import Control.Concurrent.STM
-import Control.Concurrent.MVar
 import Database.VCache.Types
+import Database.VCache.VTx
 
 -- | If you use a lot of non-durable transactions, you may wish to
 -- ensure they are synchronized to disk at various times. vcacheSync
 -- will simply wait for all transactions committed up to this point.
--- This is equivalent to running a durable read-only transaction on
--- a PVar from the involved VCache.
+-- This is equivalent to running a durable, read-only transaction.
 --
 -- It is recommended you perform a vcacheSync as part of graceful
 -- shutdown of any application that uses VCache.
 --
 vcacheSync :: VCache -> IO ()
-vcacheSync (VCache vs _) = do
-    mvWait <- newEmptyMVar
-    let onSync = putMVar mvWait ()
-    let fauxBatch = VTxBatch onSync []
-    let var = vcache_writes vs
-    let commitFauxBatch = do
-            bs <- readTVar var
-            writeTVar var (fauxBatch : bs)
-    atomically commitFauxBatch
-    takeMVar mvWait
+vcacheSync vc = runVTx (vcache_space vc) markDurable
