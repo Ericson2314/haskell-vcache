16f3b05c8df9121473e13662da549a4ad6d37b38
David Barbour <dmbarbour@gmail.com>
2015-01-31 09:58:03 -0600

    turns out LMDB conserves one page below mdb_find_oldest(), so the old RWLock is safe

--- hsrc_lib/Database/VCache/RWLock.hs
+++ hsrc_lib/Database/VCache/RWLock.hs
@@ -15,15 +15,6 @@ import Data.IORef
 import Data.IntSet (IntSet)
 import qualified Data.IntSet as IntSet
 
---
--- NOTE: I assumed earlier, based on discussion with LMDB developers on 
--- the OpenLDAP list, that LMDB supports triple buffering (preserving 
--- two snapshots). This turns out to be wrong, so I need to tweak it.
--- However, I'm looking into tweaking LMDB fit the assumptions of the
--- developers. I can later modify this to be version-adaptive.
-bUseSingleBuffer :: Bool
-bUseSingleBuffer = True
-
 -- | RWLock
 --
 -- VCache uses LMDB with the MDB_NOLOCK option, mostly because I don't
@@ -76,14 +67,6 @@ withRWLock l action = withWriterMutex l $ do
 -- rotate a fresh reader frame, and grab the oldest.
 -- Thus should only be performed while holding the writer lock.
 rotateReaderFrames :: RWLock -> IO F
-
-rotateReaderFrames l | bUseSingleBuffer = mask_ $ do
-    let var = rwlock_frames l
-    f0 <- newF
-    (FB f1 fUnused) <- takeMVar var
-    putMVar var (FB f0 fUnused)
-    return f1
-
 rotateReaderFrames l = mask_ $ do
     let var = rwlock_frames l
     f0 <- newF
@@ -91,46 +74,30 @@ rotateReaderFrames l = mask_ $ do
     putMVar var (FB f0 f1)
     return f2
 
--- 
--- Unfortunately, each writer fr
---   a writer at frame N can tolerate readers at frame N-1 but not
---   readers at frame N-2. 
-
 --
--- NOTE: Each of these 'frames' actually contains content for
--- TWO LMDB frames, since the alignment between LMDB transactions 
--- and VCache locks isn't exact. 
+-- NOTE: Each of these 'frames' actually contains readers of two
+-- transactions. Alignment between LMDB transactions and VCache
+-- RWLock isn't exact. 
 --
--- Each writer will rotate the frames just once. 
+-- Each write lock will rotate reader frames just once: 
 --
 --     (f1,f2) → (f0,f1) returning f2
 --
 -- Writer is working on frame N.
 --
--- f0 will have readers for frame N-1 and later for N.
+-- f0 will have readers for frame N-1 and (after commit) for N.
 -- f1 will have readers for frame N-2 and some for N-1.
 -- f2 will have readers for frame N-3 and some for N-2.
 --
--- After writer commits frame N, writer soon begins next frame. So,
--- there isn't much of a window for frame f0 to receive readers on
--- frame N. However, since this RWLock is not precisely aligned with
--- writer transactions, we should assume some overlap.
+-- LMDB guarantees that the data pages for frames N-1 and N-2 are 
+-- intact. However, frame N-3 will be dismantled while building
+-- frame N. Thus, we must wait for f2 readers to finish before we 
+-- begin the writer N transaction.
 --
--- According to discussion on the OpenLDAP list, LMDB keeps two frames
--- valid at all times. The writer for frame N, thus, cannot invalidate
--- the content for frames N-1 or N-2, but can overwrite content from
--- frame N-3. 
---
--- After rotate, f0 begins receiving readers for the current frame,
--- and will eventually receive content for the next frame after the
--- writer commits. Meanwhile, f1 has readers for the current frame
--- and the previous one, and f2 has readers for a previous safe frame 
--- and what will become an unsafe frame after the writer reuses some
--- 'free' pages.
---
--- If writes are sparse and fast, this reduces to double buffering.
--- But if writes are continuous, readers are effectively triple 
--- buffered.
+-- If we assume short-running readers and long-running writers, it
+-- is rare that the writer ever needs to wait on readers. Readers 
+-- never need to wait on the writer. This assumption is achieved by
+-- batching writes  in VCache.
 --
 
 
